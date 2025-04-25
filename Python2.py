6c4e92f2ce3b2163e86ad80dd2cef6d9f3bac405
Javii0 <Javierh0825@gmail.com>
2025-04-25 18:37:22 -0400

    new features
    	new file:   FinanceDataEntry.py
    	new file:   Python1.py
    	new file:   Python2.py
    	new file:   finance_data.csv
    	new file:   python3.py

---
+++ Python2.py
@@ -0,0 +1,13 @@
+#Strings 
+FirstName = "Javi"
+food = "Burger"
+print(FirstName)
+print(f"Hey {FirstName}")
+print(f"{FirstName} likes {food}")
+
+#Ints
+age = 19
+height = 74
+weight = 189
+
+print(f"Hello {FirstName} you like {food}'s and are {age} years old")
---
+++ python3.py
@@ -0,0 +1,12 @@
+age = 19
+name = "Kevin"
+pounds = 11.2
+bald = True
+age = float(age)
+print(age)
+
+name = bool(name)
+
+bald = int(bald)
+
+print(bald)
