dc053763443bdc94e2b64d4b3aa7c9ded03d07ba
Javii0 <Javierh0825@gmail.com>
2025-04-26 15:06:34 -0400

    added get transactions

--- finance_tracker.py
+++ finance_tracker.py
@@ -5,6 +5,7 @@ from FinanceDataEntry import get_date, get_amount, get_category, get_description
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    FORMAT = "%m-%d-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -26,6 +27,35 @@ class CSV:
             writer = csv.DictWriter(csvfile, fieldnames =cls.COLUMNS)
             writer.writerow(new_entry)
         print("Successful")
+    
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format = CSV.FORMAT)
+        start_date= datetime.strptime(start_date,CSV.FORMAT)
+        end_date= datetime.strptime(end_date,CSV.FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transaction found")
+        else:
+            print(f"Transaction from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+            print(filtered_df.to_string(index= False, formatters= {"date": lambda x: x.strftime(CSV.FORMAT)}))
+
+            total_income = filtered_df[filtered_df["category"] == "Income"][
+                "amount"
+                ].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"][
+                "amount"
+                ].sum()
+            print("\nSummary: ")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expanse: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2F}")
+        return filtered_df
+
 
 
 def add():
@@ -36,6 +66,7 @@ def add():
      description = get_description()
      CSV.add_entry(date, amount, category, description)         
  
-add()
+CSV.get_transactions("01-01-2025", "04-26-2025")
+#add()
 
  
